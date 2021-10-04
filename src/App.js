f328fa5a70eb4d992c2c8b562a84b1fdf1a504fa
RajanTank <rajantank731@gmail.com>
2021-10-04 16:31:26 +0530

    Changes message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Implemented CICD integration with AWS and Github action
         </p>
         <a
           className="App-link"
